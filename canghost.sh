99b9d5a6d636e8181eb4e10387f11daa2cce2589
Sourav Bagh <58340694+SouravSec@users.noreply.github.com>
2020-09-26 23:27:01 +0530

    Add files via upload

---
+++ canghost.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+#Coded by Sourav aka @itninja.official
+
+#Usage- ./Can.sh <Inferface> <NameForLogFile>
+#Example- ./Can.sh vcan0 mylog
+
+Interface="$1"
+Logfile="$2"
+
+bold="\e[1m"
+italic="\e[3m"
+reset="\e[0m"
+blink="\e[5m"
+crayn="\e[36m"
+yellow="\e[93m"
+red="\e[31m"
+green="\e[92m"
+
+
+if [ -z "$1" ]
+  then
+    echo "Interface not supplied"
+    echo "Usage : Can.sh <Inferface> <NameForLogFile>"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "Log filename not supplied"
+    echo "Usage : Can.sh <Inferface> <NameForLogFile>"
+    exit 1
+fi
+
+
+logo(){
+echo ""
+echo "               ██████████           "   
+echo "           ████          ████          "
+echo "         ██                  ██        "
+echo "       ██                      ██      "
+echo "        ██                      ██      "
+echo "      ██    ████    ████          ██    "
+echo "      ██  ██████    ██████        ██    "
+echo "      ██  ██████    ██████        ██    "
+echo "      ██  ████        ████        ██    "
+echo "      ██                          ██    "
+echo "      ██        ████      ████    ██    "
+echo "    ██  ██      ████    ██        ██    "
+echo "    ██  ▒▒██            ██  ██      ██  "
+echo "      ▒▒  ▒▒████        ████          ██"
+echo "      ▒▒  ▒▒    ██████              ██  "
+echo "      ░░░░░░          ██████████████    "
+echo "    ░░░░░░░░░░                          "
+echo -e "    ░░  ░░  ░░   ${bold}${italic}${blink}${green}GHOST IN THE CAN :${reset}        "
+echo "      ░░░░░░                            "
+echo -e "\e[31m"
+echo "  ██████ █████ ███    ██ ██████ ██   ██ ██████ ███████████████ "
+echo " ██     ██   ██████   ████      ██   ████    ████        ██    "
+echo " ██     █████████ ██  ████   ████████████    █████████   ██    "
+echo " ██     ██   ████  ██ ████    ████   ████    ██     ██   ██    "
+echo "  ████████   ████   ████ ██████ ██   ██ ██████ ███████   ██    "
+echo -e "\e[0m"                                                            
+}
+
+
+
+menu(){
+clear
+logo
+echo -e "${green}                 Coded by ${bold}SouravSec with ${red}<3${reset}"
+echo -e "${bold} --------------------------------------------------------------"
+echo -e "${red}  [01] Capture"
+echo "  [02] Replay"
+echo -e "  [09] Exit${reset}"
+read -p "  [Choose the Option] " option
+
+if [[ $option = 1 || $option = 01 ]]
+	then
+        echo -e "${green}  Dumping CAN Packets...${reset}"
+		candump -L $Interface >$Logfile.log
+		sleep 1
+                clear
+		menu
+
+	elif [[ $option = 2 || $option = 02 ]]
+	then
+        echo -e "  ${green}Sending Captured Packets...${reset} "
+	    canplayer -I $Logfile.log
+	    sleep 1
+            clear
+	    menu
+        
+        elif [[ $option = 9 || $option = 09 ]]
+                then
+                    clear
+                    exit
+
+        else
+		echo "Invalid Option..."
+		sleep 1
+		clear
+		menu
+	fi	
+}
+
+menu
+
